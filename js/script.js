136c2f70bb33e7a8efb14a82a343c82d5452b07b
kamruj-akash <source.akash@gmail.com>
2025-09-06 22:47:30 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "custom-blue": "#5A67D8", // আপনার কাস্টম কালার
+      },
+    },
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#15803D",
+        "secondary-color": "#FACC15",
+      },
+    },
+  },
+  plugins: [],
+};
