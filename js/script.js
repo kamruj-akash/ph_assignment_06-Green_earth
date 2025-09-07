5261204a2b1de2f0169e7ee6e2f2c5c31097fbf8
kamruj-akash <source.akash@gmail.com>
2025-09-07 21:35:54 +0600

    Added all section in home page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        "custom-blue": "#5A67D8", // আপনার কাস্টম কালার
-      },
-    },
-  },
-};
+const loadBtn = ()=>{
+    const url = ''
+}
