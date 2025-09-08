149151afa036fef3dc243139386b712d4460b262
kamruj-akash <source.akash@gmail.com>
2025-09-08 15:13:27 +0600

    Added all function with js

--- js/script.js
+++ js/script.js
@@ -31,18 +31,22 @@ const loadCategories = (data) => {
   data.forEach((btn) => {
     const newBtns = document.createElement("div");
     newBtns.innerHTML = `
-        <button id="activeBtn_${btn.id}"  onclick="loadViaCategory(${btn.id})"
-            class="mb-1 w-full text-left px-3 py-2 rounded-lg hover:bg-[#15803c48] cursor-pointer activeBtn"
-          >
-           ${btn.category_name}
-          </button>
+        <button
+        id="activeBtn_${btn.id}"
+        onclick="loadViaCategory(${btn.id})"
+        class="mb-1 w-full text-left px-3 py-2 rounded-lg hover:bg-[#15803c48] cursor-pointer activeBtn"
+      >
+        ${btn.category_name}
+      </button>
     `;
     btnContainer.appendChild(newBtns);
+    managerSpinner(false);
   });
 };
 
 // all plants load function
 const allPlantsLoad = () => {
+  managerSpinner(true);
   const url = "https://openapi.programming-hero.com/api/plants";
   fetch(url)
     .then((res) => res.json())
@@ -55,27 +59,31 @@ const loadAllPlants = (data) => {
   data.forEach((cards) => {
     const newCards = document.createElement("div");
     newCards.innerHTML = `
-    <div class="bg-white p-4 rounded-xl shadow">
-            <div class="h-52 bg-gray-200 rounded overflow-hidden md:h-32">
-             <img
-                class="w-full"
-                 src="${cards.image}"
-                alt=""
-              />
-            </div>
-            <h3 onclick="modalDescription(${cards.id})" class="font-semibold mt-3 hover:text-[#15803c] hover:underline hover:cursor-pointer" >${cards.name}</h3>
-            <p class="text-sm text-gray-500">
-             ${cards.description}
-            </p>
-            <div class="flex justify-between items-center mt-4">
-              <span
-                class="text-sm bg-green-100 text-green-700 px-2 py-1 rounded"
-                >${cards.category}</span
-              >
-              <span class="font-semibold">৳${cards.price}</span>
-            </div>
-                <button class="btn btn-block mt-5 bg-[#15803D] border-0 rounded-4xl shadow-none text-white">Add to Cart</button>
-          </div>
+        <div class="bg-white p-4 rounded-xl shadow">
+        <div
+          class="h-52 bg-gray-200 rounded overflow-hidden md:h-36 flex items-center justify-center"
+        >
+          <img class="w-full" src="${cards.image}" alt="" />
+        </div>
+        <h3
+          onclick="modalDescription(${cards.id})"
+          class="font-semibold mt-3 hover:text-[#15803c] hover:underline hover:cursor-pointer"
+        >
+          ${cards.name}
+        </h3>
+        <p class="text-sm text-gray-500">${cards.description}</p>
+        <div class="flex justify-between items-center mt-4">
+          <span class="text-sm bg-green-100 text-green-700 px-2 py-1 rounded"
+            >${cards.category}</span
+          >
+          <span class="font-semibold">৳${cards.price}</span>
+        </div>
+        <button
+          class="btn btn-block mt-5 bg-[#15803D] border-0 rounded-4xl shadow-none text-white hover:bg-[#24aa55]"
+        >
+          Add to Cart
+        </button>
+      </div>
     `;
     productContainer.appendChild(newCards);
     managerSpinner(false);
@@ -87,19 +95,16 @@ const loadAllPlants = (data) => {
       const newCart = document.createElement("div");
       newCart.innerHTML = `
             <div
-              class="flex justify-between items-center mb-2 bg-[#F0FDF4] py-3 px-4 cartSignle"
-            >
-              <div>
-                <h1 class="mb-1 text-[#1F2937] font-semibold">${cards.name}</h1>
-                <p>
-                  ৳<span class="item_price">${cards.price}</span>
-                 
-                </p>
-              </div>
-              <butto class="btn btn-ghost button-cart"
-                ><i class="fa-solid fa-xmark"></i
-              ></butto>
-            </div>
+        class="flex justify-between items-center mb-2 bg-[#F0FDF4] py-3 px-4 cartSignle"
+      >
+        <div>
+          <h1 class="mb-1 text-[#1F2937] font-semibold">${cards.name}</h1>
+          <p>৳<span class="item_price">${cards.price}</span></p>
+        </div>
+        <button class="btn btn-ghost button-cart">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+      </div>
      `;
       alert(`${cards.name} has been added to the cart`);
       cartContainer.appendChild(newCart);
@@ -130,6 +135,7 @@ const loadAllPlants = (data) => {
 
 // category filter function
 const loadViaCategory = (id) => {
+  // managerSpinner(true);
   const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
     .then((res) => res.json())
@@ -145,54 +151,56 @@ const loadViaCategory = (id) => {
 const displayViaCategory = (plantsData) => {
   const btnContainer = getID("produces_container");
   btnContainer.innerHTML = "";
-
   plantsData.forEach((plant) => {
     const newCards = document.createElement("div");
     newCards.innerHTML = `
-        <div class="bg-white p-4 rounded-xl shadow">
-            <div class="h-52 bg-gray-200 rounded overflow-hidden md:h-32">
-             <img
-                class="w-full"
-               src="${plant.image}"
-                alt=""
-              />
-            </div>
-            <h3 onclick="modalDescription(${plant.id})"  class="font-semibold mt-3 hover:text-[#15803c] hover:underline hover:cursor-pointer" >${plant.name}</h3>
-            <p class="text-sm text-gray-500">
-             ${plant.description}
-            </p>
-            <div class="flex justify-between items-center mt-4">
-              <span
-                class="text-sm bg-green-100 text-green-700 px-2 py-1 rounded"
-                >${plant.category}</span
-              >
-              <span class="font-semibold">৳${plant.price}</span>
-            </div>
-                <button class="btn btn-block mt-5 bg-[#15803D] border-0 rounded-4xl shadow-none text-white">Add to Cart</button>
-          </div>
+         <div class="bg-white p-4 rounded-xl shadow">
+        <div
+          class="h-52 bg-gray-200 rounded overflow-hidden md:h-36 flex items-center justify-center"
+        >
+          <img class="w-full" src="${plant.image}" alt="img" />
+        </div>
+        <h3
+          onclick="modalDescription(${plant.id})"
+          class="font-semibold mt-3 hover:text-[#15803c] hover:underline hover:cursor-pointer"
+        >
+          ${plant.name}
+        </h3>
+        <p class="text-sm text-gray-500">${plant.description}</p>
+        <div class="flex justify-between items-center mt-4">
+          <span class="text-sm bg-green-100 text-green-700 px-2 py-1 rounded"
+            >${plant.category}</span
+          >
+          <span class="font-semibold">৳${plant.price}</span>
+        </div>
+        <button
+          class="btn btn-block mt-5 bg-[#15803D] border-0 rounded-4xl shadow-none text-white hover:bg-[#24aa55]"
+        >
+          Add to Cart
+        </button>
+      </div>
     `;
     btnContainer.appendChild(newCards);
-
+    // managerSpinner(false);
     // add to cart function
     const addToCartButton = newCards.querySelector("button");
     addToCartButton.addEventListener("click", () => {
       const cartContainer = document.getElementById("cart_container");
       const newCart = document.createElement("div");
       newCart.innerHTML = `
-     <div
-              class="flex justify-between items-center mb-2 bg-[#F0FDF4] py-3 px-4 cartSignle "
-            >
-              <div>
-                <h1 class="mb-1 text-[#1F2937] font-semibold">${plant.name}</h1>
-                <p>
-                  ৳<span class="item_price">${plant.price}</span>
-                </p>
-              </div>
-              <button class="btn btn-ghost button-cart"
-                ><i class="fa-solid fa-xmark"></i
-              ></button>
-            </div>
+             <div
+        class="flex justify-between items-center mb-2 bg-[#F0FDF4] py-3 px-4 cartSignle"
+      >
+        <div>
+          <h1 class="mb-1 text-[#1F2937] font-semibold">${plant.name}</h1>
+          <p>৳<span class="item_price">${plant.price}</span></p>
+        </div>
+        <button class="btn btn-ghost button-cart">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+      </div>
      `;
+
       alert(`${plant.name} has been added to the cart`);
       cartContainer.appendChild(newCart);
       const clearBtn = newCart.querySelector(".button-cart");
@@ -233,22 +241,22 @@ const modalDescription = (id) => {
   const modalDes = (details) => {
     const newModal = document.createElement("div");
     newModal.innerHTML = `
-                <h3 class="font-semibold mb-3 text-xl">${details.name}</h3>
-                <div class="h-52 bg-gray-200 rounded overflow-hidden">
-                  <img class="w-full" alt="" />
-                </div>
-                <h3 class="font-semibold mt-3">
-                  Category: <span class="font-normal text-sm">${details.category}</span>
-                </h3>
-                <p class="text-black font-bold">
-                  Price: ৳<span class="text-gray-500 font-normal">${details.price}</span>
-                </p>
-                <p class="text-sm text-black">
-                  Description:
-                  <span class="text-gray-500 font-normal">
-                   ${details.description}</span
-                  >
-                </p>
+        <h3 class="font-semibold mb-3 text-xl">${details.name}</h3>
+      <div
+        class="h-72 bg-gray-200 rounded overflow-hidden flex items-center justify-center"
+      >
+        <img class="w-full" src="${details.image}" alt="img" />
+      </div>
+      <h3 class="font-semibold mt-3">
+        Category: <span class="font-normal text-sm">${details.category}</span>
+      </h3>
+      <p class="text-black font-bold">
+        Price: ৳<span class="text-gray-500 font-normal">${details.price}</span>
+      </p>
+      <p class="text-sm text-black">
+        Description:
+        <span class="text-gray-500 font-normal"> ${details.description}</span>
+      </p>
   `;
     modalContainer.appendChild(newModal);
   };
